5a952f195a9821e6339e54b4b3d78ce81055a9b6
MalBourbie <MalBourbie@gmail.com>
2024-05-30 15:02:18 +0200

    Récupération du code d'autorisation puis du token d'accès. Stocker dans le local storage provisoirement

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
 
 function App() {
+  const client_secret = ""
+
+  const pathname = window.location.pathname
+  const verification = pathname.split("?")
+  const next = verification[0]
+  console.log(next)
+
+  if (next === "/exchange_token") {
+    // récupération du code d'autorisation
+    const urlParams = new URLSearchParams(window.location.search)
+    const code = urlParams.get("code")
+    localStorage.setItem("code", code)
+    // Récupération de l'ACCESS TOKEN
+    fetch(`https://www.strava.com/oauth/token?client_id=127497&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`, {
+      method: "POST",
+    })
+    .then(response => response.json())
+    .then(data => {
+      const access_token = data.access_token
+      access_token === undefined ? localStorage.setItem("undefined", access_token) : localStorage.setItem("access_token", access_token)
+    })
+
+    window.location.href = "http://localhost:3000/"
+  }
+
   // test GET à l'api Strava des données de l'athlete
   const StravaFetch = () => {
+    const access_token = localStorage.getItem("access_token")
     fetch("https://www.strava.com/api/v3/athlete", {
       method: "GET",
       headers: {
-        Authorization: "Bearer 9abaa7d4245b66e1d82fe6f5576fb5c065035620"
+        Authorization: `Bearer ${access_token}`
       }
     })
     .then(response => response.json()).then(data => console.log(data))
   }
 
   const redirectionLogin = () => {
-    window.location.href = "http://www.strava.com/oauth/authorize?client_id=127497&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read"
+    window.location.href = "http://www.strava.com/oauth/authorize?client_id=127497&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read"
   }
+
 
   return (
     <div>
       <header>
         <a
-          className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           Learn React
         </a>
@@ -30,6 +54,7 @@ function App() {
       <main>
         <button onClick={() => redirectionLogin()}>Connexion avec Strava</button>
         <button onClick={() => StravaFetch()}>Info athlete</button>
+        <span>test</span>
       </main>
     </div>
   );
