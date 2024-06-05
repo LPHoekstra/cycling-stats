26b93a2a25aad7a3e760792a5b61811ea4396225
MalBourbie <MalBourbie@gmail.com>
2024-06-05 14:40:33 +0200

    suppressions de la gestion des connections qui est maintenant sur un serveur

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,22 @@
-
 function App() {
-  const client_secret = ""
-
-  const pathname = window.location.pathname
-  const verification = pathname.split("?")
-  const next = verification[0]
-  console.log(next)
-
-  if (next === "/exchange_token") {
-    // récupération du code d'autorisation
-    const urlParams = new URLSearchParams(window.location.search)
-    const code = urlParams.get("code")
-    localStorage.setItem("code", code)
-    // Récupération de l'ACCESS TOKEN
-    fetch(`https://www.strava.com/oauth/token?client_id=127497&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`, {
-      method: "POST",
+  // données de l'athlete
+  const StravaFetch = () => {
+    fetch("https://localhost:4000/athlete", {
+      method: "GET",
+      credentials: "include"
     })
     .then(response => response.json())
-    .then(data => {
-      const access_token = data.access_token
-      access_token === undefined ? localStorage.setItem("undefined", access_token) : localStorage.setItem("access_token", access_token)
-    })
-
-    window.location.href = "http://localhost:3000/"
-  }
-
-  // test GET à l'api Strava des données de l'athlete
-  const StravaFetch = () => {
-    const access_token = localStorage.getItem("access_token")
-    fetch("https://www.strava.com/api/v3/athlete", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${access_token}`
-      }
-    })
-    .then(response => response.json()).then(data => console.log(data))
+    .then(data => console.log(data))
   }
 
   const redirectionLogin = () => {
-    window.location.href = "http://www.strava.com/oauth/authorize?client_id=127497&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read"
+    window.location.href = "http://www.strava.com/oauth/authorize?client_id=127497&response_type=code&redirect_uri=https://localhost:4000/exchange_token&approval_prompt=force&scope=read"
   }
 
 
   return (
     <div>
-      <header>
-        <a
-          href="https://reactjs.org"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
       <main>
         <button onClick={() => redirectionLogin()}>Connexion avec Strava</button>
         <button onClick={() => StravaFetch()}>Info athlete</button>
